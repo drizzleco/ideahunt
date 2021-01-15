d3243555105acebe6555695d98bd6426020009cc
Herrick Fang <herrickfang@gmail.com>
2021-01-15 00:35:28 -0800

    Add universal link prefix

--- client/navigation/LinkingConfiguration.ts
+++ client/navigation/LinkingConfiguration.ts
@@ -1,7 +1,7 @@
 import * as Linking from "expo-linking";
 
 export default {
-  prefixes: [Linking.makeUrl("/")],
+  prefixes: [Linking.makeUrl("/"), "https://ideahunt.onrender.com"],
   config: {
     screens: {
       Root: {
