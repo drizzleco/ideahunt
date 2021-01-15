d91b88194372da92981f2ea353280611c1c4f5e0
Herrick Fang <herrickfang@gmail.com>
2021-01-15 00:54:39 -0800

    Remove old libraries

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider } from "@apollo/client";
 import { client } from "./graphql/Client";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
